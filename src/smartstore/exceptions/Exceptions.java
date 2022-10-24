38eddd7871b19e5354523b4145cd51c69f1c76fd
lala9663 <lala96632040@gmail.com>
2022-10-25 01:34:22 +0900

    feat: 예외처리 추가

--- src/smartstore/exceptions/Exceptions.java
+++ src/smartstore/exceptions/Exceptions.java
@@ -2,19 +2,44 @@ package smartstore.exceptions;
 
 public class Exceptions {
     public static boolean check(String input, int rangeMin, int rangeMax){
-
-        // 숫자로만 썼는지 확인
         for (int i = 0; i <input.length(); i++) {
             if (input.charAt(i) < '0' || input.charAt(i) > '9'){
-                System.out.printf("똑바로 입력해!");
+                System.out.printf("예외처리 숫자만 입력해주세요.\n");
                 return false;
             }
         }
     return true;
     }
 
+
+    // 이름 확인
+    public static boolean checkName(String input) {
+        for (int i = 0; i <input.length(); i++) {
+            if (input.matches("^[a-zA-Z]*$") == false){
+                System.out.printf("예외처리 5자 이상,영어만 입력해주세요.");
+                return false;
+            } else if (input.matches("^[a-zA-Z]*$") == true) {
+                return  true;
+            }
+        }
+        return true;
+    }
+
+    // ID 확인
+    public static boolean checkID(String input) {
+        for (int i = 0; i <input.length(); i++) {
+            if (input.matches("^[a-zA-Z0-9]*$") == false){
+                System.out.printf("예외처리 5자 이상,영어와 숫자만 입력해주세요.");
+                return false;
+            } else if (input.matches("^[a-zA-Z0-9]*$") == true) {
+                return  true;
+            }
+        }
+        return true;
+    }
+
     //입력받은 문자열이 숫자라면 true, 반대인 경우 false
-    public static boolean checkStringType(String input) {
+    public static boolean update(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) < '0' || input.charAt(i) > '9')
                 return false;
@@ -36,4 +61,5 @@ public class Exceptions {
             return 0;
         }
     }
+
 }
